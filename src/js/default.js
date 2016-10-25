8cec4f7788d40ed5f5b164a3b7d84ef49cfb1830
Jelle de Jong <jelle@twijg-it.nl>
2016-10-25 09:46:41 +0200

    Uitgaande links tracken

--- src/js/default.js
+++ src/js/default.js
@@ -14,9 +14,32 @@ var scroll = function(id) {
   }, 1000);
 };
 
+/**
+* Functie die klikken op een uitgaande link in Analytics bijhoudt.
+* Deze functie vereist een geldige URL-tekenreeks als argument en gebruikt de betreffende URL-tekenreeks
+* als eventlabel. Wanneer de methode 'transport' wordt ingesteld op 'beacon', wordt de hit verzonden
+* bij gebruik van 'navigator.sendBeacon' in de browser die dat ondersteunt.
+*/
+var trackOutboundLink = function(url) {
+  ga('send', 'event', 'outbound', 'click', url, {
+    'transport': 'beacon',
+    'hitCallback': function() {
+      document.location = url;
+    }
+  });
+};
+
 $(function() {
   $('nav .logo a, nav .main a').on('click', function() {
     scroll($(this).attr('href'));
     return false;
+  });
+
+  $("a[href^='http'],a[href^='mailto']").each(function() {
+    var link = $(this);
+    link.on('click', function() {
+      trackOutboundLink(this.href);
+      return false;
+    });
   });
 });
